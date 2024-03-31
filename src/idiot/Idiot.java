357f1ac9d653cd95feee26ee79bae8a573856a0c
SMOLKEYS <SMOLKEYS@users.noreply.github.com>
2024-03-31 17:12:23 +0800

    stupiide

---
+++ src/idiot/Idiot.java
@@ -0,0 +1,20 @@
+package idiot;
+
+import arc.util.*;
+import mindustry.*;
+import idiot.util.*;
+
+public class Idiot{
+
+	public Idiot(){
+		Log.info("idiot stupid dumb");
+	}
+
+	public static void idiota(){
+		Vars.ui.showInfo("you idiota grahhhhh");
+	}
+
+	public static void callout(){
+		IUtils.broken();
+	}
+}
---
+++ src/idiot/util/IUtils.java
@@ -0,0 +1,8 @@
+package idiot.util;
+
+public class IUtils{
+
+	public static void broken(){
+		throw new RuntimeException("this is broken idiota");
+	}
+}
